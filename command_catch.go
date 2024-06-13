51b75fd12c25ef1a557d5cc7c04344ba9afd70cc
ericmariot <eric.mariot@betrybe.com>
2024-06-13 20:23:34 -0300

    feat: add cache to the caught command

--- command_catch.go
+++ command_catch.go
@@ -23,6 +23,7 @@ func callbackCatch(cfg *config, args ...string) error {
 		return fmt.Errorf("failed to catch %s", pokemonName)
 	}
 
+	cfg.caughtPokemon[pokemonName] = pokemon
 	fmt.Printf("%s was caught!", pokemonName)
 
 	return nil
